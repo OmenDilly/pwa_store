86962dba25138e3c78ef977b209d7c6b2895622a
OmenDilly <59134777+OmenDilly@users.noreply.github.com>
2021-06-30 20:57:55 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App flex-1">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
